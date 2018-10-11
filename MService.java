3c40db17404f9f00dc4d02c387021dc21f18a317
sunny <hk109@126.com>
2018-10-11 11:31:40 +0800

    add java

---
+++ MService.java
@@ -0,0 +1,16 @@
+package com.offcn.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.offcn.po.Mobile;
+
+public interface MService {
+	@GET
+	@Path("/find/{number}")
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public Mobile find(@PathParam("number")String number);
+}
